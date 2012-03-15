389e1d0af21aca566cbcfcb6f078a07a1a2829f2
Bártfai Tamás <bartfaitamas@gmail.com>
2012-03-15 08:22:45 +0100

    PHP, JS and twig settings.

--- personal/el-get-config.el
+++ personal/el-get-config.el
@@ -10,6 +10,12 @@
         (:name geben
                :description "A remote debugging environment for Emacs."
                :type elpa)
+        (:name js2-mode
+               :website "https://github.com/mooz/js2-mode"
+               :type github
+               :description "An improved JavaScript editing mode"
+               :pkgname "mooz/js2-mode"
+               :prepare (autoload 'js2-mode "js2-mode" nil t))
         (:name php-mode
                :website "https://github.com/ejmr/php-mode"
                :description "Major mode for editing PHP code"
@@ -18,7 +24,7 @@
 
 (setq my-packages
       (append
-       '(el-get magithub)
+       '(el-get magithub js2-mode)
        (mapcar 'el-get-source-name el-get-sources)))
 
 (el-get 'sync my-packages)
--- personal/php-config.el
+++ personal/php-config.el
@@ -1,4 +1,7 @@
-(require 'symfony-mode)
-
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.html.twig$" . django-html-mode))
+
+(add-hook 'php-mode-hook
+          '(lambda ()
+             (setq comment-start "//"
+                   comment-end "")))
