2f3b29ef9fa7e85de9bd92476c7cfeefb50b2e3f
Bártfai Tamás <bartfaitamas@gmail.com>
2012-03-24 14:15:04 +0100

    JSON fix for js3-mode

---
+++ personal/json-fix.el
@@ -0,0 +1,23 @@
+(add-to-list 'auto-mode-alist '("\\.json$" . js3-mode))
+(add-to-list 'auto-mode-alist '("\\.json.twig$" . js3-mode))
+
+(defadvice js2-reparse (before json)
+        (setq js2-buffer-file-name buffer-file-name))
+(ad-activate 'js2-reparse)
+
+(defadvice js2-parse-statement (around json)
+        (if (and (= tt js2-LC)
+                        js2-buffer-file-name
+                        (or
+                         (string-equal (substring js2-buffer-file-name -5) ".json")
+                         (string-equal (substring js2-buffer-file-name -10) ".json.twig"))
+                        (eq (+ (save-excursion
+                                                (goto-char (point-min))
+                                                (back-to-indentation)
+                                                (while (eolp)
+                                                        (next-line)
+                                                        (back-to-indentation))
+                                                (point)) 1) js2-ts-cursor))
+                (setq ad-return-value (js2-parse-assign-expr))
+                ad-do-it))
+(ad-activate 'js2-parse-statement)
